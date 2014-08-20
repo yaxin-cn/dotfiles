d993b2a07e9f8c732f06bb109cf10fe865325518
yaxin <yaxin.me@gmail.com>
2014-08-20 10:43:50 +0800

    edit install.s

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 current_dir=`pwd`
 home_dir=$HOME
+dotfiles_dir=$home_dir"/.dotfiles"
 trash_dir=$home_dir"/.trash/"
-config_files="bashrc gitconfig vimrc npmrc"
+config_files="bashrc gitconfig vimrc npmrc pip"
 
 if [ ! -d $trash_dir ];then
     mkdir $trash_dir
@@ -11,10 +12,9 @@ fi
 
 for file in ${config_files};do
     config_file=$home_dir"/."$file
-    if [ -f $config_file ];then
+    if [ -f $config_file ] || [ -d $config_file ];then
         mv $config_file $trash_dir
     fi
 
-    ln -s $current_dir"/"$file $config_file
+    ln -s $dotfiles_dir"/"$file $config_file
 done
-
