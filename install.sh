23b901396c9b13f2e1e950c57fc34a8195723fa9
yaxin <yaxin.me@gmail.com>
2015-10-13 09:37:06 +0800

    add log level info

--- install.sh
+++ install.sh
@@ -6,6 +6,14 @@ if [ -z $BASH ];then
     echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] Please use bash rather than sh to execute this script."
     exit 1
 fi
+# |Level|Example|
+# ------|-------
+# |Fatal|Highest level: important stuff down|
+# |Error|For example application crashes / exceptions.|
+# |Warn|Incorrect behavior but the application can continue |
+# |Info|Normal behavior like mail sent, user updated profile etc.|
+# |Debug|Executed queries, user authenticated, session expired|
+# |Trace|Begin method X, end method X etc|
 
 
 log() {
