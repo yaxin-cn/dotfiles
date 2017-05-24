17bdc95f25b2a5e647e442adc59149337da7b39d
yaxin <yaxin.me@gmail.com>
2017-05-24 15:42:37 +0800

    bugfix

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,7 +7,7 @@ declare -A LINK_MAP
 LINK_MAP=([$MODULE_DIR/zshrc]=$INSTALL_PATH/.zshrc [$MODULE_DIR]=$INSTALL_PATH/.zshrc.d)
 LINK_MAP["$MODULE_DIR/zsh_aliases"]="$INSTALL_PATH/.zsh_aliases"
 LINK_MAP["$MODULE_DIR/zshenv"]="$INSTALL_PATH/.zshenv"
-LINK_MAP["$MODULE_DIR"]="$INSTALL_PATH/.zsh"
+LINK_MAP["$MODULE_DIR"]="$INSTALL_PATH/.zshrc.d"
 
 for src in "${!LINK_MAP[@]}";do
     linkit "$src" "${LINK_MAP[$src]}"
